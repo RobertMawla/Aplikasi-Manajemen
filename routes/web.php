390195965f7fb6aff1067ec3dc9305c3494b5ad5
Robert <robertmawlarobbany@gmail.com>
2025-04-27 11:36:11 +0700

    part4

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TugasController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('welcome');
+
+
+Route::get('login',[AuthController::class,'login'])->name('login');
+Route::post('login',[AuthController::class,'loginProses'])->name('loginProses');
+
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
+
+
+Route::middleware('checkLogin')->group(function(){
+
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+Route::get('user',[UserController::class,'index'])->name('user');
+Route::get('tugas',[TugasController::class,'index'])->name('tugas');
 });
 
-Route::get('dashboard',[DashboardController::class,'index']);
+
